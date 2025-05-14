b7c66d2cfe9200a7c4a24c57d77b501148b91d01
arushipa <147722747+arushipa@users.noreply.github.com>
2025-05-14 15:34:22 +0530

    changes

--- app.py
+++ app.py
@@ -12,36 +12,30 @@ def home():
 def funfactnumber():
     data = request.get_json()
 
-    response_text = str(data)
+    #response_text = str(data)
     #Today's History
 
     #Surprise Me
 
     #date
-    '''value = data['option']
-    if str(value) == "1":
+    if "button_index" not in data:
+        value = -1
+    else:
+        value = data['button_index']
+
+
+
+    if str(value) == "0":
         print("Surprise Me")
-        innerwrapper =  data["text"]
-        optionentered = innerwrapper["body"]
-        if(optionentered == "trivia"):
-            response = requests.get("http://numbersapi.com/random/trivia")
-            response_text = response.text
-        elif(optionentered == "math"):
-            response = requests.get("http://numbersapi.com/random/math")
-            response_text = response.text
-        elif (optionentered == "date"):
-            response = requests.get("http://numbersapi.com/random/date")
-            response_text = response.text
-        elif (optionentered == "year"):
-            response = requests.get("http://numbersapi.com/random/year")
-            response_text = response.text
+        response = requests.get("http://numbersapi.com/random/trivia")
+        response_text = response.text
+    elif str(value) == "1":
+        print("Option History")
+
+        
     elif str(value) == "2":
         print("Option Number")
-        print("Option Date")
-        numberwrapper = data["text"]
-        numberentered = numberwrapper["body"]
-        response = requests.get("http://numbersapi.com/" + str(numberentered) )
-        response_text = response.text
+        response_text = "Enter a number"
     elif str(value) == "3":
         print("Option Date")
         datewrapper = data["text"]
@@ -49,10 +43,15 @@ def funfactnumber():
         date_object = datetime.strptime(dateentered,'%Y-%m-%d')
         response = requests.get("http://numbersapi.com/" + str(date_object.month)+"/"+str(date_object.day)+"/date")
         response_text = response.text
-    elif str(value) == "4":
-        print("Option History")
+    elif str(value) == "-1":
+        print("Number details")
+        print("Option num")
+        numberwrapper = data["text"]
+        numberentered = numberwrapper["body"]
+        response = requests.get("http://numbersapi.com/" + str(numberentered))
+        response_text = response.text
     else:
-        print("Please select an option")'''
+        print("Please select an option")
 
     #senddatatochatbot(response.text)
     url = "https://v1-api.swiftchat.ai/api/bots/0210276432749689/messages"
@@ -67,7 +66,7 @@ def createbuttons():
     url = "https://v1-api.swiftchat.ai/api/bots/0210276432749689/messages"
     api_key = "21bda582-e8d0-45bc-bb8b-a5c6c555d176"
     headers = {"Authorization": f"Bearer {api_key}", 'Content-type': 'application/json'}
-    payload= "{\n    \"to\": \"+919764772960\",\n    \"type\": \"button\",\n    \"button\": {\n        \"body\": {\n            \"type\": \"text\",\n            \"text\": {\n                \"body\": \"Hello, click on the button to explore Number Nuggets.\"\n            }\n        },\n        \"buttons\": [\n            {\n                \"type\": \"solid\",\n                \"body\": \"Trivia\",\n                \"reply\": \"Mathematics, Class 1\"\n            },\n            {\n                \"type\": \"solid\",\n                \"body\": \"Mathematics quiz, Class 1\",\n                \"reply\": \"Mathematics quiz, Class 1\"\n            },\n            {\n                \"icon\": \"registration\",\n                \"type\": \"dotted\",\n                \"body\": \"Add another student\",\n                \"reply\": \"Add another student\"\n            }\n        ],\n        \"allow_custom_response\": true,\n        \"ttl\": 600\n    }\n}"
+    payload = "{\n    \"to\": \"+919764772960\",\n    \"type\": \"button\",\n    \"button\": {\n        \"body\": {\n            \"type\": \"text\",\n            \"text\": {\n                \"body\": \"Hello, click on the button to explore Number Nuggets.\"\n            }\n        },\n        \"buttons\": [\n            {\n                \"type\": \"solid\",\n                \"body\": \"Number Trivia\",\n                \"reply\": \"Number Trivia\"\n            },\n            {\n                \"type\": \"solid\",\n                \"body\": \"Today History\",\n                \"reply\": \"Today History \"\n            },\n            {\n            \"type\": \"solid\",\n                \"body\": \"Know about a number\",\n                \"reply\": \"Know about a number\"\n            }\n        ],\n        \"allow_custom_response\": true,\n        \"ttl\": 600\n    }\n}"
 
     responsenew = requests.request("POST", url, headers=headers, data=payload)
     return jsonify(responsenew.text)
